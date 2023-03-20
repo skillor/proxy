5cf5a7b8ab2fb0a2bab4651edbb65a442982abde
skillor <skillor@gmx.net>
2023-03-20 20:58:57 +0100

    refactor

--- proxy.py
+++ proxy.py
@@ -1,32 +1,4 @@
 import os
-try:
-    import parse as parser
-except ModuleNotFoundError:
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    parse_path = os.path.join(cwd, 'parse.py')
-    config_path = os.path.join(cwd, 'config.ini')
-    if not os.path.exists(parse_path):
-        print('creating', parse_path)
-        with open(parse_path, 'w', encoding='utf-8') as f:
-            f.write('''def parse(data, proxy):
-    print('{}[{}://{}:{}]{}'.format(proxy.origin, proxy.protocol, proxy.id, proxy.port, data))
-    return data
-''')
-        import parse as parser
-
-    if not os.path.exists(config_path):
-        print('creating', config_path)
-        with open(config_path, 'w', encoding='utf-8') as f:
-            f.write('''HOST=127.0.0.1:8080
-PROXY=http://www.httpvshttps.com:80
-; HOST=0.0.0.0
-; PROXY=udp://217.160.58.45:4004
-; PROXY=tcp://217.160.58.45:4004
-; DNS=8.8.8.8
-''')
-        print('stopping, change your', config_path, 'and restart the script')
-        quit()
-
 from urllib3.util import connection
 import urllib3.exceptions
 
@@ -39,43 +11,6 @@ import threading
 
 from importlib import reload
 
-DNS_CACHE = {}
-DNS_RESOLVER = None
-
-
-def init_dns_resolver(custom_dns):
-    if custom_dns is None or len(custom_dns) == 0:
-        return
-    global DNS_RESOLVER
-    if DNS_RESOLVER is None:
-        import dns.resolver
-        DNS_RESOLVER = dns.resolver.Resolver(configure=False)
-    DNS_RESOLVER.nameservers = custom_dns
-
-
-def resolve_hostname(hostname):
-    if DNS_RESOLVER is None:
-        return hostname
-
-    if hostname in DNS_CACHE:
-        return DNS_CACHE[hostname]
-
-    _t = DNS_RESOLVER.resolve(hostname, 'A')[0].to_text()
-    DNS_CACHE[hostname] = _t
-    return _t
-
-
-_orig_create_connection = connection.create_connection
-
-
-def patched_create_connection(address, *args, **kwargs):
-    hostname, port = address
-    hostname = resolve_hostname(hostname)
-    return _orig_create_connection((hostname, port), *args, **kwargs)
-
-
-connection.create_connection = patched_create_connection
-
 RUNNING_I = 0
 
 
@@ -85,17 +20,9 @@ def get_id():
     return RUNNING_I - 1
 
 
-def parse(data, proxy):
-    try:
-        reload(parser)
-        return parser.parse(data, proxy)
-    except Exception as e:
-        print('{}[{}://{}:{}]{}'.format(proxy.origin, proxy.protocol, proxy.id, proxy.port, e))
-    return data
-
-
 class ProxyWare:
-    def __init__(self, host, port, protocol, origin='unknown'):
+    def __init__(self, handler, host, port, protocol, origin):
+        self.handler = handler
         self.id = get_id()
         self.port = port
         self.host = host
@@ -116,11 +43,11 @@ os.environ['WERKZEUG_RUN_MAIN'] = 'true'
 
 
 class ProxyHttp(ProxyWare, threading.Thread):
-    def __init__(self, from_host, from_port, to_host, to_port, protocol):
+    def __init__(self, handler, from_host, from_port, to_host, to_port, protocol):
         threading.Thread.__init__(self, daemon=True)
-        ProxyWare.__init__(self, to_host, to_port, protocol, 'proxy')
-        self.g2p = ProxyWare(from_host, from_port, protocol, 'client')
-        self.p2s = ProxyWare(to_host, to_port, protocol, 'server')
+        ProxyWare.__init__(self, handler, to_host, to_port, protocol, 'proxy')
+        self.g2p = ProxyWare(handler, from_host, from_port, protocol, 'client')
+        self.p2s = ProxyWare(handler, to_host, to_port, protocol, 'server')
         self.app = Flask(__name__)
         logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
@@ -145,7 +72,7 @@ class ProxyHttp(ProxyWare, threading.Thread):
                 'form': dict(request.form),
                 'data': request.data,
             }
-            req_info = parse(req_info, self.g2p)
+            req_info = self.handler.parse(req_info, self.g2p)
             requester = {
                 'GET': requests.get,
                 'POST': requests.post,
@@ -166,7 +93,7 @@ class ProxyHttp(ProxyWare, threading.Thread):
                 'data': res_data,
                 'status_code': res.status_code,
             }
-            res_info = parse(res_info, self.p2s)
+            res_info = self.handler.parse(res_info, self.p2s)
             resp = make_response(res_data, res.status_code, res_info['headers'])
             return resp
 
@@ -179,9 +106,9 @@ class ProxyHttp(ProxyWare, threading.Thread):
 
 
 class Proxy2Server(ProxyWare, threading.Thread):
-    def __init__(self, host, port, protocol):
+    def __init__(self, handler, host, port, protocol):
         threading.Thread.__init__(self, daemon=True)
-        ProxyWare.__init__(self, host, port, protocol, 'server')
+        ProxyWare.__init__(self, handler, host, port, protocol, 'server')
         self.game = None
         if self.protocol == 'udp':
             pass
@@ -203,16 +130,16 @@ class Proxy2Server(ProxyWare, threading.Thread):
                 while True:
                     data = self.socket.recv(4096)
                     if data:
-                        data = parse(data, self)
+                        data = self.handler.parse(data, self)
                         self.game.conn.sendall(data)
             except ConnectionAbortedError or ConnectionResetError:
                 self.lost_connection()
 
 
 class Game2Proxy(ProxyWare, threading.Thread):
-    def __init__(self, host, port, protocol):
+    def __init__(self, handler, host, port, protocol):
         threading.Thread.__init__(self, daemon=True)
-        ProxyWare.__init__(self, host, port, protocol, 'client')
+        ProxyWare.__init__(self, handler, host, port, protocol, 'client')
         self.server = None
         if self.protocol == 'udp':
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -239,19 +166,19 @@ class Game2Proxy(ProxyWare, threading.Thread):
                 if client_address is None:
                     client_address = address
                 if address == self.server.address:
-                    data = parse(data, self.server)
+                    data = self.handler.parse(data, self.server)
                     self.socket.sendto(data, client_address)
                     client_address = None
                 else:
                     client_address = address
-                    data = parse(data, self)
+                    data = self.handler.parse(data, self)
                     self.socket.sendto(data, self.server.address)
         else:
             try:
                 while True:
                     data = self.conn.recv(4096)
                     if data:
-                        data = parse(data, self)
+                        data = self.handler.parse(data, self)
                         self.server.socket.sendall(data)
             except ConnectionResetError or ConnectionAbortedError:
                 self.lost_connection()
@@ -260,9 +187,9 @@ class Game2Proxy(ProxyWare, threading.Thread):
 
 
 class ProxyTCPUDP(ProxyWare, threading.Thread):
-    def __init__(self, from_host, from_port, to_host, to_port, protocol):
+    def __init__(self, handler, from_host, from_port, to_host, to_port, protocol):
         threading.Thread.__init__(self, daemon=True)
-        ProxyWare.__init__(self, to_host, to_port, protocol, 'proxy')
+        ProxyWare.__init__(self, handler, to_host, to_port, protocol, 'proxy')
         self.from_host = from_host
         self.from_port = from_port
         self.protocol = protocol
@@ -271,8 +198,8 @@ class ProxyTCPUDP(ProxyWare, threading.Thread):
 
     def setup(self):
         ProxyWare.setup(self)
-        self.g2p = Game2Proxy(self.from_host, self.from_port, self.protocol)
-        self.p2s = Proxy2Server(self.host, self.port, self.protocol)
+        self.g2p = Game2Proxy(self.handler, self.from_host, self.from_port, self.protocol)
+        self.p2s = Proxy2Server(self.handler, self.host, self.port, self.protocol)
         self.g2p.server = self.p2s
         self.p2s.game = self.g2p
 
@@ -300,32 +227,68 @@ def host_to_ip_port(h):
     return ':'.join(s[:-1]), port, protocol
 
 
-def main(host=('127.0.0.1',), proxy=('127.0.0.1:8080',), dns=tuple()):
-    """
-    :param host: list of hosts
-    :param proxy: list of proxy server to create
-    :param dns: list of custom dns server ['8.8.8.8']
-    """
-    host_count = len(host)
-    dns = list(dns)
-    init_dns_resolver(dns)
-    for i, remote in enumerate(proxy):
-        local_host, local_port, _ = host_to_ip_port(host[min(host_count - 1, i)])
-        remote_host, remote_port, protocol = host_to_ip_port(remote)
-        if local_port < 0:
-            local_port = remote_port
-        if protocol in ['tcp', 'udp']:
-            t = ProxyTCPUDP(local_host, local_port, resolve_hostname(remote_host), remote_port, protocol)
-        elif protocol in ['http', 'https']:
-            t = ProxyHttp(local_host, local_port, resolve_hostname(remote_host), remote_port, protocol)
-        else:
-            raise Exception('unknown protocol: ' + protocol)
-        t.start()
+class ProxyHandler(threading.Thread):
+    def __init__(self, parse, host=('127.0.0.1',), proxy=('127.0.0.1:8080',), dns=tuple()):
+        """
+        :param parse: function to parse the data parse(data, proxy: ProxyWare)
+        :param host: list of hosts
+        :param proxy: list of proxy server to create
+        :param dns: list of custom dns server ['8.8.8.8']
+        """
+        threading.Thread.__init__(self, daemon=True)
+        self.parse = parse
+        self.host = host
+        self.proxy = proxy
+        self.dns = list(dns)
+        self.dns_cache = None
+        self.dns_resolver = None
 
-    while True:
-        cmd = input('$ ')
-        if cmd == 'q' or cmd == 'quit':
-            exit(0)
+    def resolve_hostname(self, hostname):
+        if self.dns_resolver is None:
+            return hostname
+
+        if hostname in self.dns_cache:
+            return self.dns_cache[hostname]
+
+        _t = self.dns_resolver.resolve(hostname, 'A')[0].to_text()
+        self.dns_cache[hostname] = _t
+        return _t
+
+    def run(self):
+        if self.dns is None or len(self.dns) == 0:
+            pass
+        else:
+            import dns.resolver
+            self.dns_resolver = dns.resolver.Resolver(configure=False)
+            self.dns_resolver.nameservers = self.dns
+
+            _orig_create_connection = connection.create_connection
+
+            def patched_create_connection(address, *args, **kwargs):
+                hostname, port = address
+                hostname = self.resolve_hostname(hostname)
+                return _orig_create_connection((hostname, port), *args, **kwargs)
+
+            connection.create_connection = patched_create_connection
+
+        host_count = len(self.host)
+        for i, remote in enumerate(self.proxy):
+            local_host, local_port, _ = host_to_ip_port(self.host[min(host_count - 1, i)])
+            remote_host, remote_port, protocol = host_to_ip_port(remote)
+            if local_port < 0:
+                local_port = remote_port
+            if protocol in ['tcp', 'udp']:
+                t = ProxyTCPUDP(self, local_host, local_port, self.resolve_hostname(remote_host), remote_port, protocol)
+            elif protocol in ['http', 'https']:
+                t = ProxyHttp(self, local_host, local_port, self.resolve_hostname(remote_host), remote_port, protocol)
+            else:
+                raise Exception('unknown protocol: ' + protocol)
+            t.start()
+
+        while True:
+            cmd = input('$ ')
+            if cmd == 'q' or cmd == 'quit':
+                exit(0)
 
 
 def parse_config(file_path):
@@ -344,7 +307,52 @@ def parse_config(file_path):
     return d
 
 
-if __name__ == '__main__':
+def start_proxy(*args, **kwargs):
+    p = ProxyHandler(*args, **kwargs)
+    p.run()
+    return p
+
+
+def main():
+    try:
+        import parse as parser
+    except ModuleNotFoundError:
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        parse_path = os.path.join(cwd, 'parse.py')
+        config_path = os.path.join(cwd, 'config.ini')
+        if not os.path.exists(parse_path):
+            print('creating', parse_path)
+            with open(parse_path, 'w', encoding='utf-8') as f:
+                f.write('''def parse(data, proxy):
+        print('{}[{}://{}:{}]{}'.format(proxy.origin, proxy.protocol, proxy.id, proxy.port, data))
+        return data
+    ''')
+            import parse as parser
+
+        if not os.path.exists(config_path):
+            print('creating', config_path)
+            with open(config_path, 'w', encoding='utf-8') as f:
+                f.write('''HOST=127.0.0.1:8080
+    PROXY=http://www.httpvshttps.com:80
+    ; HOST=0.0.0.0
+    ; PROXY=udp://217.160.58.45:4004
+    ; PROXY=tcp://217.160.58.45:4004
+    ; DNS=8.8.8.8
+    ''')
+            print('stopping, change your', config_path, 'and restart the script')
+            quit()
+
+    def _parse(data, proxy):
+        try:
+            return parser.parse(data, proxy)
+        except Exception as e:
+            print('{}[{}://{}:{}]{}'.format(proxy.origin, proxy.protocol, proxy.id, proxy.port, e))
+        return data
+
     cwd = os.path.dirname(os.path.realpath(__file__))
     config_path = os.path.join(cwd, 'config.ini')
-    main(**parse_config(config_path))
+    start_proxy(_parse, **parse_config(config_path))
+
+
+if __name__ == '__main__':
+    main()
